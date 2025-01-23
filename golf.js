085bc9a09a2fb0eee407e062d11934ba2e644d54
Nick VanBrunt <NVanBrunt@kennedy-center.org>
2025-01-23 15:25:09 -0500

    Lighting and texture

--- golf.js
+++ golf.js
@@ -42,8 +42,10 @@ function Game() {
         strokes: 0,
 
         materials: {
-            ground: null
+            green: null,
+            bumper: null
         },
+        shadows:[],
         init: async function() {
             const canvas = document.getElementById("canvas");
 
@@ -53,9 +55,14 @@ function Game() {
                 disableWebGL2Support: false });
             this.scene = new BABYLON.Scene(this.engine);
         
-            // create a light aiming at 0,1,0
-            const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this.scene);
-            light.intensity = 0.7; // dim light
+            //const light1 = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this.scene);
+            const light1 = new BABYLON.DirectionalLight("light", new BABYLON.Vector3(0, -5, 5), this.scene);
+            const light2 = new BABYLON.DirectionalLight("light", new BABYLON.Vector3(5, -4, 1), this.scene);
+            light1.intensity = 0.7; // dim light
+            light2.intensity = 0.7; // dim light
+
+            //this.shadows.push(new BABYLON.ShadowGenerator(1024, light1));
+            this.shadows.push(new BABYLON.ShadowGenerator(1024, light2));
         
             // create a camera
             this.camera = new BABYLON.ArcRotateCamera("camera", Math.PI/4, Math.PI/4, 10, new BABYLON.Vector3(0,0,0));   
@@ -67,13 +74,20 @@ function Game() {
             this.scene.enablePhysics(new BABYLON.Vector3(0, this.globals.gravity, 0), new BABYLON.HavokPlugin(true, havok));
         
             // create materials
-            this.materials.ground = new BABYLON.StandardMaterial("groundMat");
-            this.materials.ground.diffuseColor = new BABYLON.Color3(0, .8, 0);
+            this.materials.green = new BABYLON.StandardMaterial("greenmat");
+            this.materials.green.diffuseColor = new BABYLON.Color3(.6, .8, .6);
+            this.materials.green.diffuseTexture = new BABYLON.Texture("/assets/green-seamless.jpg");
+            this.materials.green.diffuseTexture.uScale = 1;
+            this.materials.green.diffuseTexture.vScale = 1;
+
+            this.materials.bumper = new BABYLON.StandardMaterial("bumpermat");
+            this.materials.bumper.diffuseColor = new BABYLON.Color3(.2, .4, .15);
+
 
 
             this.bumperRoot = BABYLON.MeshBuilder.CreateBox("bumper", {
                 height: this.globals.bumperHeight, width:this.globals.tileSize, depth: 1}, this.scene);
-            this.bumperRoot.material = this.materials.ground;
+            this.bumperRoot.material = this.materials.bumper;
             this.bumperRoot.isVisible = false;
         },
         addBumper: function(x, y, z, rotation) {
@@ -91,7 +105,8 @@ function Game() {
                 width: this.globals.tileSize, 
                 height: this.globals.tileSize }, this.scene);
             ground.position = new BABYLON.Vector3(x, y, z);
-            ground.material = this.materials.ground;
+            ground.material = this.materials.green;
+            ground.receiveShadows = true;
             if (pitch != 0) {
                 ground.rotation = new BABYLON.Vector3(pitch, 0, 0);
             }
@@ -119,6 +134,18 @@ function Game() {
             return ground;
         },
         addCorner: function(x, y, z, rotation) {
+            const ground = BABYLON.MeshBuilder.CreateGround("ground", { 
+                width: this.globals.tileSize, 
+                height: this.globals.tileSize }, this.scene);
+            ground.position = new BABYLON.Vector3(x, y, z);
+            ground.material = this.materials.green;
+            ground.receiveShadows = true;
+
+            new BABYLON.PhysicsAggregate(ground, BABYLON.PhysicsShapeType.MESH, { 
+                mass: 0, 
+                restitution: 0, 
+                friction: 0 }, this.scene);
+
             const box = BABYLON.MeshBuilder.CreateBox("box", { 
                 width: this.globals.tileSize+1, 
                 height: this.globals.bumperHeight,
@@ -127,7 +154,7 @@ function Game() {
             box.material = this.materials.ground;
             
             const cylinder = BABYLON.MeshBuilder.CreateCylinder("tube", {
-                height:10, 
+                height:15, 
                 diameterTop:this.globals.tileSize * 2-1, 
                 diameterBottom:this.globals.tileSize * 2-1, 
                 tessellation:64, 
@@ -141,9 +168,10 @@ function Game() {
             var corner = boxCSG.subtract(cylinderCSG).toMesh("corner", null, this.scene);
             corner.position = new BABYLON.Vector3(x, y, z);
             corner.rotation = new BABYLON.Vector3(0, rotation, 0);
-            corner.material = this.materials.ground;
+            corner.material = this.materials.bumper;
+            corner.receiveShadows = true;
 
-            const aggregate = new BABYLON.PhysicsAggregate(corner, BABYLON.PhysicsShapeType.MESH, { 
+            new BABYLON.PhysicsAggregate(corner, BABYLON.PhysicsShapeType.MESH, { 
                 mass: 0, 
                 restitution: 0, 
                 friction: 0 }, this.scene);
@@ -161,7 +189,7 @@ function Game() {
                         height:this.globals.bumperHeight/2, 
                         diameterTop:size, 
                         diameterBottom:size, 
-                        tessellation:24, 
+                        tessellation:size < 20 ? 24 : 32, 
                         subdivisions:1
                     }, this.scene);
                     physicsShape = BABYLON.PhysicsShapeType.CYLINDER;
@@ -175,7 +203,7 @@ function Game() {
                 break; 
             }
             mesh.position = new BABYLON.Vector3(x, this.globals.bumperHeight/4, z);
-            mesh.material = this.materials.ground;
+            mesh.material = this.materials.bumper;
             const aggregate = new BABYLON.PhysicsAggregate(mesh, physicsShape, { 
                 mass: 0, 
                 restitution: 0, 
@@ -196,6 +224,9 @@ function Game() {
                 friction: this.globals.friction }, this.scene);
             aggregate.body.setLinearDamping(game.globals.damping);
             this.ball.body = aggregate.body;
+            for(let i=0; i<this.shadows.length; i++) {
+                this.shadows[i].getShadowMap().renderList.push(ball);
+            }
 
             return ball;
         },
@@ -217,7 +248,8 @@ function Game() {
 
             var hole = groundCSG.subtract(cylinderCSG).toMesh("hole", null, this.scene);
             hole.position = mesh.position;
-            hole.material = this.materials.ground;
+            hole.material = this.materials.green;
+            hole.receiveShadows = true;
 
             const aggregate = new BABYLON.PhysicsAggregate(hole, BABYLON.PhysicsShapeType.MESH, { 
                 mass: 0, 
@@ -298,7 +330,7 @@ function Game() {
         strike: function() {
             if (this.ball.stopped) {
                 this.renderAimLine = false;
-                //this.disposeAimLine();
+                this.disposeAimLine();
                 clearInterval(this.aimLineInterval);
                 let a = this.getImpulseAmount()
                 let angle = this.camera.alpha + Math.PI;
